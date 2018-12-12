024beda5e1ee2ae2f9b90f8da47ff063b4e7525d
byronkamal <byronkamal@hotmail.com>
2018-12-12 12:05:39 -0200

    Fixing file's name

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,5 @@ const express = require('express')
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => res.render('auth/signup'))
+routes.get('/', (req, res) => res.render('auth/signup.njk'))
 module.exports = routes
