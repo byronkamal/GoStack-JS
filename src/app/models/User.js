ba97c9ad1f2d3acbeea5a56157b981272f4f6852
byronkamal <byronkamal@hotmail.com>
2018-12-15 19:27:51 -0200

    Updating User model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,10 +1,22 @@
+const bcrypt = require('bcryptjs')
+
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     avatar: DataTypes.STRING,
+    password: DataTypes.VIRTUAL,
     password_hash: DataTypes.STRING,
     Provider: DataTypes.BOOLEAN
+  },
+  {
+    hooks: {
+      beforeSave: async user => {
+        if (user.password) {
+          user.password_hash = await bcrypt(user.password, 8)
+        }
+      }
+    }
   })
 
   return user
