0519a740803f7c25031cd6724dd4375503e74187
byronkamal <byronkamal@hotmail.com>
2018-12-26 17:34:35 -0200

    Fixing parameter and adding password encryption

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,23 +1,30 @@
 const bcrypt = require('bcryptjs')
 
 module.exports = (sequelize, DataTypes) => {
-  const user = sequelize.define('user', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    password: DataTypes.VIRTUAL,
-    password_hash: DataTypes.STRING,
-    Provider: DataTypes.BOOLEAN
-  },
-  {
-    hooks: {
-      beforeSave: async user => {
-        if (user.password) {
-          user.password_hash = await bcrypt(user.password, 8)
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING,
+      provider: DataTypes.BOOLEAN
+    },
+    {
+      hooks: {
+        beforeSave: async user => {
+          if (user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8)
+          }
         }
       }
     }
-  })
+  )
 
-  return user
+  User.prototype.checkPassword = function (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
+
+  return User
 }
